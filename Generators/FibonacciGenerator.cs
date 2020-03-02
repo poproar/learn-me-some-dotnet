4202187599ed9f4f65239607f7db352e5ae3498a
poproar <poproar@users.noreply.github.com>
2020-03-02 14:48:44 -0800

    move classes to generators namespace

--- Generators/FibonacciGenerator.cs
+++ Generators/FibonacciGenerator.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-namespace Hello
+namespace Generators
 {
     public class FibonacciGenerator
     {
--- Generators/RandomDieGenerator.cs
+++ Generators/RandomDieGenerator.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Text;
 
-namespace Hello
+namespace Generators
 {
     public class RandomDieGenerator
     {
