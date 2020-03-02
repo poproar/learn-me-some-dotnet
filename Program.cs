af626831c7a8b53331fcc6f983a239928c27cc11
poproar <poproar@users.noreply.github.com>
2020-03-02 10:13:00 -0800

    after dotnet new

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_me_some_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what a terrible namespace, but let's get started:");
+        }
+    }
+}
