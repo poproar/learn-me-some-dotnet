57ba880f11bfd64f0339ef8137d41d1b258b4f22
poproar <poproar@users.noreply.github.com>
2020-03-02 10:19:57 -0800

    build it

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace learn_me_some_dotnet
         static void Main(string[] args)
         {
             Console.WriteLine("what a terrible namespace, but let's get started:");
+            Console.WriteLine("\n after running a dotnet build I found the file in ./bin/Debug/netcoreapp3.1/namespace\n");
+            Console.WriteLine(".... and it ran. Yes!");
+
+
         }
     }
 }
